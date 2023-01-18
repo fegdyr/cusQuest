ae4f88b808079a097e09fc050bfbc0b70dbce74f
sten <lennver@gmail.com>
2023-01-18 22:02:19 +0100

    Boilerplate and beginning

---
+++ src/components/Question.vue
@@ -0,0 +1,28 @@
+<template>
+    <h3>Question {{ question.number }} </h3>
+    <p>{{ question.text }}</p>
+    <li v-for="(option, index) in question.answerOptions">
+        <button value="{{ index }}">option</button>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            question: {
+                number: 3,
+                text: "How many bolts do the bridge to Öland have?",
+                answerOptions: [
+                    "2000",
+                    "20000",
+                    "200000",
+                    "2000000",
+                    "20000000"
+                ],
+                answerIndex: 3
+            }
+        }
+    },
+}
+</script>
